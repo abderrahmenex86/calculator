8f1db7fb74f927a59d58d4223de877ae81af32f9
Abderrahmene Benounene <a.benounene@esi-sba.dz>
2021-10-02 20:51:29 +0100

    Get a functional calculator tho no keyboard support yet

--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,113 @@
+const history = document.querySelector('#history')
+const current = document.querySelector('#current')
+const buttons = document.querySelectorAll('button')
+
+let currentValue = 0
+let previousValue = 0
+let opr = ""
+let signF = false
+
 function add() {
-    let total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        total += arguments[i]
-    }
-    return total
+    return arguments[0] + arguments[1]
 }
 
 function subtract() {
-    let total = arguments[0];
-    for (let i = 1; i < arguments.length; i++) {
-        total -= arguments[i]
-    }
-    return total
+    return arguments[1] - arguments[0]
 }
 
 function multiply() {
-    let total = 1;
-    for (let i = 0; i < arguments.length; i++) {
-        total *= arguments[i]
-    }
-    return total
+    return arguments[0] * arguments[1]
 }
 
 function divide() {
-    let total = arguments[0];
-    if (arguments[1] === 0) {
-        return Infinity
-    }
-    for (let i = 1; i < arguments.length; i++) {
-        total /= arguments[i]
-    }
-    return total
+    return arguments[1] / arguments[0]
 }
 
 function operate(opr, num1, num2) {
     if (opr === "+") {
         return add(num1, num2)
-    }
-    if (opr === "-") {
+    } else if (opr === "-") {
         return subtract(num1, num2)
-    }
-    if (opr === "*") {
+    } else if (opr === "*") {
         return multiply(num1, num2)
-    }
-    if (opr === "/") {
+    } else if (opr === "/") {
         return divide(num1, num2)
     }
 }
 
-let displayValue = 0;
-
-const result = document.querySelector('#result')
-const previous = document.querySelector('#previous')
-previous.textContent = "0"
-previousValue = 0
-let buttonClicked = ""
-
-function myInput(c){
-    if (parseFloat(c) >= 0 || parseFloat(c) <= 9) {
-        displayValue += c
-    }
-    else if (c !== "=") {
-        buttonClicked = c
-        previousValue = displayValue
-        displayValue = "0"
-        previous.textContent = previousValue
-    }
-    else {
-        displayValue = operate(buttonClicked, parseInt(previousValue), parseInt((displayValue)))
-        console.log('hi')
-    }
-    result.textContent = displayValue
+function start() {
+    current.textContent = 0
+    history.textContent = current.textContent
 }
-const buttons = document.querySelectorAll('button')
+
+
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-            if (result.textContent === "0") {
-                result.textContent = ""
-                displayValue = ""
+        if (current.textContent.length < 15) {
+            if (e.target.value < 10 && e.target.value >= 0) {
+                if (current.textContent === "0") {
+                    current.textContent = ""
+                }
+                current.textContent += e.target.value
+                currentValue = parseFloat(current.textContent)
+            } else if (e.target.value == "+") {
+                previousValue = currentValue
+                history.textContent = previousValue
+                currentValue = 0
+                current.textContent = currentValue
+                opr = "+"
+            } else if (e.target.value == "-") {
+                previousValue = currentValue
+                history.textContent = previousValue
+                currentValue = 0
+                current.textContent = currentValue
+                opr = "-"
+            } else if (e.target.value == "*") {
+                previousValue = currentValue
+                history.textContent = previousValue
+                currentValue = 0
+                current.textContent = currentValue
+                opr = "*"
+            } else if (e.target.value == "/") {
+                previousValue = currentValue
+                history.textContent = previousValue
+                currentValue = 0
+                current.textContent = currentValue
+                opr = "/"
+            } else if (e.target.value == "=") {
+                if (opr !== "") {
+                    history.textContent = currentValue
+                    currentValue = operate(opr, currentValue, previousValue)
+                    current.textContent = currentValue
+                    opr = ""
+                }
+            } else if (e.target.value == "clear") {
+                previousValue = 0
+                history.textContent = previousValue
+                currentValue = 0
+                current.textContent = currentValue
+                signF = false
+                opr = ""
+            } else if (e.target.value == "sign") {
+                current.textContent = "-" + currentValue
+                currentValue = parseFloat(current.textContent)
+            } else if (e.target.value == ".") {
+                if (!signF) {
+                    current.textContent = currentValue + "."
+                    currentValue = parseFloat(current.textContent)
+                    console.log(currentValue)
+                    signF = true
+                }
+            } else if (e.target.value === "backspace") {
+                if (current.textContent.length > 1) {
+                    current.textContent = current.textContent.slice(0, current.textContent.length - 1)
+                }
+                else if (current.textContent.length === 1) {
+                    current.textContent = 0
+                }
             }
-            myInput(e.target.value)
         }
-    )
+    })
 })
+
+start()
